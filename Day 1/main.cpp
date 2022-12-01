80217f1faf664ba5d0d82c79005ef9ae232a3f22
crime-story <73616883+crime-story@users.noreply.github.com>
2022-12-01 14:27:27 +0200

    added solution for day 1

---
+++ Day 1/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+ifstream fin("inputFile.in");
+ofstream fout("inputFile.out");
+
+int main() {
+    string s;
+    vector<int> elvesCaloriesSum;
+    int index = 0;
+    elvesCaloriesSum.push_back(index);
+
+    while (!fin.eof()) {
+        getline(fin, s);
+        if (s != "") {
+            elvesCaloriesSum[index] += atoi(s.c_str());
+        } else {
+            index++;
+            elvesCaloriesSum.push_back(0);
+        }
+    }
+    fin.close();
+
+    int maxCaloriesSumElf1 = 0;
+    int maxCaloriesSumElf2 = 0;
+    int maxCaloriesSumElf3 = 0;
+
+    for (auto &elfCaloriesSum: elvesCaloriesSum) {
+        if (elfCaloriesSum > maxCaloriesSumElf1) {
+            maxCaloriesSumElf3 = maxCaloriesSumElf2;
+            maxCaloriesSumElf2 = maxCaloriesSumElf1;
+            maxCaloriesSumElf1 = elfCaloriesSum;
+        } else if (elfCaloriesSum > maxCaloriesSumElf2) {
+            maxCaloriesSumElf3 = elfCaloriesSum;
+            maxCaloriesSumElf2 = elfCaloriesSum;
+        } else if (elfCaloriesSum > maxCaloriesSumElf3) {
+            maxCaloriesSumElf3 = elfCaloriesSum;
+        }
+    }
+
+    int sumCaloriesFirst3Elves = maxCaloriesSumElf1 + maxCaloriesSumElf2 + maxCaloriesSumElf3;
+
+    fout << maxCaloriesSumElf1 << " " << maxCaloriesSumElf2 << " " << " " << maxCaloriesSumElf3 << "\n";
+    fout << sumCaloriesFirst3Elves;
+
+    fout.close();
+    return 0;
+}
